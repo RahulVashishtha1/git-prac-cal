5cf12faaeaddecaa4514adb4f5d093678564acd3
Rahul Vashishtha <68159064+RahulVashishtha1@users.noreply.github.com>
2023-11-18 10:26:15 +0530

    Multiplication and divide feature added

--- cal.c
+++ cal.c
@@ -1,18 +1,23 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int n;
-    int a,b;
+    int a, b;
     printf("Enter first number: ");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("Enter second number: ");
-    scanf("%d",&b);
-    printf("Enter the operation you want to do: \n 1. Addition");
+    scanf("%d", &b);
+    printf("Enter the operation you want to do: \n 1. Addition.\n 2. Multiplication.\n3. Divide");
     switch (n)
     {
     case 1:
-        printf("The addition of %d and %d is %d",a,b,a+b);
+        printf("The addition of %d and %d is %d", a, b, a + b);
         break;
-    
+    case 2:
+        printf("The multiplication of %d and %d is %d", a, b, a * b);
+    case 3:
+        printf("The division of %d and %d is %d", a, b, a / b);
+
     default:
         printf("Enter valid choice.");
         break;
