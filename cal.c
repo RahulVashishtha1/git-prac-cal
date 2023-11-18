f5c63139cca138bf00db6a041265092ceed43239
Rahul Vashishtha <68159064+RahulVashishtha1@users.noreply.github.com>
2023-11-18 10:28:42 +0530

    Added subtraction feature

--- cal.c
+++ cal.c
@@ -6,13 +6,14 @@ int main(){
     scanf("%d",&a);
     printf("Enter second number: ");
     scanf("%d",&b);
-    printf("Enter the operation you want to do: \n 1. Addition");
+    printf("Enter the operation you want to do: \n 1. Addition\n2. Subtraction");
     switch (n)
     {
     case 1:
         printf("The addition of %d and %d is %d",a,b,a+b);
         break;
-    
+    case 2:
+        printf("The subtraction of %d and %d is %d",a,b,a-b);
     default:
         printf("Enter valid choice.");
         break;
